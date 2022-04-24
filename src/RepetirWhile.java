b99afbf5d55901a3e3dc43b2c044206c1dec6d5c
juniordeveloper11 <89851068+juniordeveloper11@users.noreply.github.com>
2022-04-24 20:43:03 -0300

    Initial commit

---
+++ src/RepetirWhile.java
@@ -0,0 +1,9 @@
+public class RepetirWhile {
+
+    public static void main(String[] args) {
+        for ( int  numero = 10; numero >= 0 ; numero --) {
+            System.out.println(" numero atual é :" + numero );
+        }
+    }
+
+}
